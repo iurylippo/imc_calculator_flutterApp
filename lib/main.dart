429d54e49088ff3c493fc167e9e13f1de96fd184
iuryvieira <iuryvieira.lippo@gmail.com>
2019-04-13 12:01:55 -0300

    change scaffold style and add a refresh button

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,12 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Calculadora de IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh),
+            onPressed: () {},)
+        ],
       ),
     );
   }
